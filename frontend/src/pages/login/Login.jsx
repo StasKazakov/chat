86792962528d407e62294bb9ee7e659bb611a09f
StasKazakov <urgor8@gmail.com>
2024-09-23 15:32:48 +0300

    connect back and front

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
     return (
         <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
@@ -18,9 +20,9 @@ const Login = () => {
                         </label>
                         <input type="password" placeholder="Enter Pasword" className="w-full input input-bordered h-10" />
                     </div>
-                    <a href="#" className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block">
+                    <Link to='/signup' className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block">
                         {"Don't"} have an account?
-                    </a>
+                    </Link>
                     <button className="btn btn-block btn-sm mt-2">Login</button>
                 </form>
             </div>
@@ -30,36 +32,5 @@ const Login = () => {
 
 export default Login;
 
-// STARTER CODE FOR THIS FILE
-// const Login = () => {
-//     return (
-//         <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
-//             <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-//                 <h1 className='text-3xl font-semibold text-center text-gray-300'>
-//                     Login <span className="text-blue-500">ChatApp </span>
-//                 </h1>
-//                 <form>
-//                     <div>
-//                         <label className="label p-2">
-//                             <span className="text-base lsbel-text">Username</span>
-//                         </label>
-//                         <input type="text" placeholder="Enter username" className="w-full input input-bordered h-10" />
-//                     </div>
-//                     <div>
-//                         <label className="label p-2">
-//                             <span className="text-base label-text">Password</span>
-//                         </label>
-//                         <input type="password" placeholder="Enter Pasword" className="w-full input input-bordered h-10" />
-//                     </div>
-//                     <a href="#" className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block">
-//                         {"Don't"} have an account?
-//                     </a>
-//                     <button className="btn btn-block btn-sm mt-2">Login</button>
-//                 </form>
-//             </div>
-//         </div>
-//     );
-// };
 
-// export default Login;
 
