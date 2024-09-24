8d060299739bb41f11e5cee4ffe8d60e59ffe803
StasKazakov <urgor8@gmail.com>
2024-09-24 18:00:18 +0300

    logining finish

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -13,7 +13,7 @@ const SignUp = () => {
     gender: ''
   })
 
-  const {loding, signup} = useSignup()
+  const {loading, signup} = useSignup()
 
   const handleCheckboxChange = (gender) => {
     SetInputs({...inputs, gender})
@@ -95,7 +95,10 @@ const SignUp = () => {
                 </Link>
 
                 <div>
-                  <button className='btn btn-block btn-sm mt-2 border border-slate-700'>Sign Up</button>
+                  <button className='btn btn-block btn-sm mt-2 border border-slate-700'
+                  disabled={loading}>
+                    {loading ? <span className="loading loading-spinner"></span> : "Sign up"}
+                    </button>
                 </div>
               </form>
           </div>
